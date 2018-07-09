1e93fb3e9cc750939203187bce0dc369241414ba
Mizuki Murase <40147995+murase-pluslab@users.noreply.github.com>
2018-07-09 19:27:44 +0900

    Update main.py

--- camera/main.py
+++ camera/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, Response
-from processor.simple_streamer import SimpleStreamer as VideoCamera
+# from processor.simple_streamer import SimpleStreamer as VideoCamera
 # from processor.pedestrian_detector import PedestrianDetector as VideoCamera
 # from processor.motion_detector import MotionDetector as VideoCamera
 # from processor.qr_detector import QRDetector as VideoCamera
