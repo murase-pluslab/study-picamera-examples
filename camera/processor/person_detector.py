aa5198dea28d007d066bb21bd36b653c99785bba
Mizuki Murase <40147995+murase-pluslab@users.noreply.github.com>
2018-07-09 20:22:42 +0900

    Update person_detector.py

--- camera/processor/person_detector.py
+++ camera/processor/person_detector.py
@@ -58,6 +58,7 @@ class PersonDetector(object):
             y = startY - 15 if startY - 15 > 15 else startY + 15
             cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)    
             persons += 1
+            cv2.putText(frame,'{} person',(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
         
         if persons > 0:
             timestamp = datetime.now()
